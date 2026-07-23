a849545ee238b87b8d796b97a229796ce3a34141
pmdasilva <pmdasilvaestud@gmail.com>
2026-07-23 20:16:53 -0300

    feat-resposividade

--- env-react/src/Components/Header.tsx
+++ env-react/src/Components/Header.tsx
@@ -1,7 +1,7 @@
 export function Header() {
     return (
         <>
-            <header className="sm:flex justify-center text-center bg-gray-700 opacity-85 h-20 font-semibold items-center">
+            <header className="sm:w-full flex justify-center text-center bg-gray-700 opacity-85 h-20 font-semibold items-center">
                 <h1 className="text-white text-4xl">GitFinder</h1>
             </header>
         </>
--- env-react/src/Components/ListUsers.tsx
+++ env-react/src/Components/ListUsers.tsx
@@ -2,20 +2,21 @@ type ListUsersProps = {
     title: string
     description: string
     url?: string
+    className?: string
 }
 
-export default function ListUsers({ title, description, url }: ListUsersProps) {
+export default function ListUsers({ title, description, url, className }: ListUsersProps) {
     return (
-        <div className="mb-5 mt-5">
+        <div className={`sm:mb-5 mt-5 p-5 text-wrap w-auto ${className}`}>
             {url ? (
-                <a className="text-2xl text-blue-300 mt-3 mb-3 font-bold" href={url} target="_blank" rel="noreferrer">
+                <a className="sm: uppercase text-gray-200 font-bold" href={url} target="_blank" rel="noreferrer">
                     {title}
                 </a>
             ) : (
-                <strong className="text-2xl text-blue-300 mt-3 mb-3">{title}</strong>
+                <strong className="sm: capitalize text-blue-300 mt-3 mb-3">{title}</strong>
             )}
-            <p className="text-2xl text-gray-500 mt-3 mb-3">{description}</p>
-            <hr />
+            <p className="sm: text-gray-200 mt-3 mb-3">{description}</p>
+
         </div>
     )
 }
